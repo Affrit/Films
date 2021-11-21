48b232e026793742fd973b84d1bb6ab25407b555
Affrit <79639177+Affrit@users.noreply.github.com>
2021-11-21 11:05:19 +0200

    genreList and sortList are taken out to constants

--- src/components/Layouts/Sider/MoviesSider.jsx
+++ src/components/Layouts/Sider/MoviesSider.jsx
@@ -6,36 +6,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getMoviesPageData, setSortParamAC, setGenreListAC } from '../../../store/actions/moviesPageActions';
 import { Select } from 'antd';
 import { useState } from 'react';
+import { GENRE_LIST, SORT_PARAMS } from '../../../constants/constants';
 
 const { Option } = Select;
 const { SubMenu } = Menu
 
-const genreList = [
-  { id: 28, name: 'Action' },
-  { id: 12, name: 'Adventure' },
-  { id: 16, name: 'Animation' },
-  { id: 35, name: 'Comedy' },
-  { id: 80, name: 'Crime' },
-  { id: 99, name: 'Documentary' },
-  { id: 18, name: 'Drama' },
-  { id: 10751, name: 'Family' },
-  { id: 14, name: 'Fantasy' },
-  { id: 36, name: 'History' },
-  { id: 27, name: 'Horror' },
-  { id: 10402, name: 'Music' },
-  { id: 9648, name: 'Mystery' },
-  { id: 10749, name: 'Romance' },
-  { id: 878, name: 'Science Fiction' },
-  { id: 10770, name: 'TV Movie' },
-  { id: 53, name: 'Thriller' },
-  { id: 10752, name: 'War' },
-  { id: 37, name: 'Western' },
-]
+const genreOptions = GENRE_LIST.map(genre => {
+  return <Option key={genre.id}>{genre.name}</Option>
+})
 
-const children = [];
-for (const genre of genreList) {
-  children.push(<Option key={genre.id}>{genre.name}</Option>);
-}
+const sortOptions = SORT_PARAMS.map(sortItem => {
+  return <Option key={sortItem.id}>{sortItem.name}</Option>
+})
 
 export const MoviesSider = (props) => {
   const { with_genres, sort_by } = useSelector(({ moviesPage: { filtrationOptions: { with_genres, sort_by } } }) => ({
@@ -66,10 +48,7 @@ export const MoviesSider = (props) => {
         <SubMenu key="sort" icon={<SearchOutlined />} title="Sort by" style={{}}>
           <Menu.Item key="sort-select" style={{ width: '100%', padding: '10px' }}>
             <Select defaultValue={sort_by} onChange={onChangeSort}>
-              <Option value="popularity.desc">Popularity(descending)</Option>
-              <Option value="popularity.asc">Popularity(ascending)</Option>
-              <Option value="vote_average.desc">Rating(descending)</Option>
-              <Option value="vote_average.asc">Rating(ascending)</Option>
+              {sortOptions}
             </Select>
           </Menu.Item>
         </SubMenu>
@@ -84,7 +63,7 @@ export const MoviesSider = (props) => {
               <div style={{ textAlign: 'center' }}>genre</div>
               <div>
                 <Select mode="tags" style={{ width: '100%' }} placeholder="Tags Mode" value={with_genres} onChange={onChangeGenres}>
-                  {children}
+                  {genreOptions}
                 </Select>
               </div>
             </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,38 @@
 export const BASE_URL = 'https://api.themoviedb.org/3'
 export const BASE_URL_IMG = 'https://image.tmdb.org/t/p/w500'
 export const API_KEY = 'api_key=d213a0b8d4c80c1b639c6fe4b9cb34ac'
+
+export const GENRE_LIST = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+]
+
+export const SORT_PARAMS = [
+  { id: 'popularity.desc', name: 'Popularity(descending)' },
+  { id: 'popularity.asc', name: 'Popularity(ascending)' },
+  { id: 'vote_average.desc', name: 'Rating(descending)' },
+  { id: 'vote_average.asc', name: 'Rating(ascending)' },
+  { id: 'release_date.desc', name: 'Release(descending)' },
+  { id: 'release_date.asc', name: 'Release(ascending)' },
+  { id: 'original_title.desc', name: 'Name(descending)' },
+  { id: 'original_title.asc', name: 'Name(ascending)' },
+  { id: 'revenue.desc', name: 'revenue(descending)' },
+  { id: 'revenue.asc', name: 'revenue(ascending)' },
+]
